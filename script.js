80d18cdbb85041360be3df99347cfe23f2ed0481
pho <phonum5@gmail.com>
2022-08-08 16:28:52 +0000

    added and tested operator functions

---
+++ script.js
@@ -0,0 +1,34 @@
+function add(firstNum, secondNum) {
+  return firstNum + secondNum;
+}
+
+function subtract(firstNum, secondNum) {
+  return firstNum - secondNum;
+}
+
+function multiply(firstNum, secondNum) {
+  return firstNum * secondNum;
+}
+
+function divide(firstNum, secondNum) {
+  return firstNum / secondNum;
+}
+
+function operate(operator, num1, num2) {
+  switch (operator) {
+    case "+":
+      add(num1, num2);
+      break;
+    case "-":
+      subtract(num1, num2);
+      break;
+    case "*":
+      multiply(num1, num2);
+      break;
+    case "/":
+      divide(num1, num2);
+      break;
+    default:
+      break;
+  }
+}
